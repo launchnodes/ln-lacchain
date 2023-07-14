528c0113ec0ec43df39f0b0d7e89acf96340e9c0
ravinayagam <137376650+ravinayagam@users.noreply.github.com>
2023-07-14 23:32:01 +0530

    err fix

--- deploy.sh
+++ deploy.sh
@@ -46,10 +46,15 @@ function CreateBesuNode() {
   kubectl apply -f $deploy_net
   echo "It may take 2 to 3 mins for the Pods Availability,  sleeping for 150 sec"
   while [ "$(kubectl get pods -n $NAME_SPACE -l=app='besu-node-writer' -o jsonpath='{.items[*].status.containerStatuses[0].started}')" != "true" ]; do    sleep 10;  echo "Waiting for pod to be ready."; done
+  echo -e "Get Pods Status...\n"
   kubectl get pods -n $NAME_SPACE
   sleep 20;
+  echo -e "Your Enode Address...\n"
   kubectl exec -it pod/besu-node-writer-0 -n $NAME_SPACE  -c writer-nginx -- curl -X POST --data '{"jsonrpc":"2.0","method":"net_enode","params":[],"id":1}' http://localhost:4545
-
+  echo ""
+  sleep 1;
+  echo -e "Your Wallet Address...\n"
+  kubectl exec -it pod/besu-node-writer-0 -n $NAME_SPACE  -c writer-nginx -- curl -X POST --data '{"jsonrpc":"2.0","method":"eth_coinbase","params":[],"id":53}' http://127.0.0.1:4545
 }
 
 function k8sAccess() {
@@ -74,25 +79,16 @@ function registerNetwork() {
     kubectl exec -it pod/besu-node-writer-0 -n $NAME_SPACE  -c writer-besu -- sh -c  "echo $ADDRESS>>/data/besu/permission.txt"
     
     arrIN=(${ADDRESS//'"result" :'/ }) 
-    ADDRESS2=${arrIN[0]}
+    declare -g ADDRESS2=${arrIN[0]}
     arrIN=(${ENODE//'"result" :'/ }) 
-    ENODE2=${arrIN[0]}
+    declare -g ENODE2=${arrIN[0]}
     
-    if [ "$1" == "mainnet" ]; then
-      echo "Contact our premeier launchnode support team for mainnet"
-    elif [ "$1" == "testnet" ]; then
-      curl --location --request POST 'https://api.backoffice.lac-net.net/market' --header 'Content-Type: application/json' --data-raw '{ "market":"AWS", "network":"Open Protestnet", "membership":"Premium", "address":'$ADDRESS2', "enode":'$ENODE2'}' --insecure
-    else
-      echo -e "Invalid Network $deploy_net selected / Not a valid network provided \n 
-                #### Select only from 'mainnet' or 'testnet' ####"
-    fi
 
     echo ""
-    echo -e "Share the ENODE address to LACChain Network team via email 'tech.support@lacnet.com' \n
-          You have to restart the services after Access granted by LACChain Team"
+    echo -e "Share the ENODE address to LACChain Network team via email 'tech.support@lacnet.com'
+    \n You have to restart the services after Access granted by LACChain Team"
     echo ""
 }
-
 
 
 function selectNetwork() {
@@ -104,6 +100,8 @@ function selectNetwork() {
       replaceVal
       CreateBesuNode
       registerNetwork
+      echo "Registering Node..."
+      echo "Contact our premeier launchnode support team for mainnet registration"
     elif [ "$1" == "testnet" ]; then
       deploy_net=$testnet
       echo $deploy_net
@@ -112,9 +110,12 @@ function selectNetwork() {
       replaceVal
       CreateBesuNode
       registerNetwork
+      echo -e "Registering Node...\n"
+      curl --location --request POST 'https://api.backoffice.lac-net.net/market' --header 'Content-Type: application/json' --data-raw '{ "market":"AWS", "network":"Open Protestnet", "membership":"Premium", "address":'$ADDRESS2', "enode":'$ENODE2'}' --insecure
     else
-      echo -e "Invalid Network $deploy_net selected / Not a valid network provided \n 
-                #### Select only from 'mainnet' or 'testnet' ####"
+
+      echo -e "Invalid Network $deploy_net selected / Not a valid network provided \n
+                \n #### Select only from 'mainnet' or 'testnet' ####"
     fi
 
 }
